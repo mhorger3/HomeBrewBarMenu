3939b7616c28dadc04434c7ea72a76a64b71423b
Matthew Horger <mhorger3@gmail.com>
2026-07-06 11:07:09 -0400

    adding mobile implementation

--- src/app/components/display/display.ts
+++ src/app/components/display/display.ts
@@ -2,15 +2,16 @@ import {
   Component,
   OnInit,
   inject,
-  signal
+  signal,
+  computed
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BeerRow } from '../beer-row/beer-row';
-
+import { Beer } from '../../models/beer';
 import { SlideService } from '../../services/slide.service';
 import { LayoutService } from '../../services/layout.service';
 import { BeerSection } from '../../models/beer-section';
-
+import { LayoutModeService } from '../../services/layout-mode.service';
 @Component({
   selector: 'app-display',
   standalone: true,
@@ -25,16 +26,54 @@ export class Display implements OnInit {
 
   private slideService = inject(SlideService);
   private layout = inject(LayoutService);
-
+  private layoutMode = inject(LayoutModeService);
+  readonly searchText = signal('');
+  readonly selectedCategory = signal('All');
+  readonly isMobile = this.layoutMode.isMobile;
   // All grouped/paginated beer sections
   readonly sections = signal<BeerSection[]>([]);
 
   // Current page for each category
   readonly pageIndex = signal<Record<string, number>>({});
-
   // Used for CSS transitions
   readonly transitioning = signal(false);
+  readonly filteredSections = computed(() => {
 
+  const search = this.searchText().trim().toLowerCase();
+  const category = this.selectedCategory();
+
+  return this.sections()
+    .filter(section =>
+      category === 'All' || section.title === category
+    )
+    .map(section => ({
+
+      ...section,
+
+      beers: section.pages
+        .flat()
+        .filter(beer => {
+
+          if (!search) {
+            return true;
+          }
+
+          return (
+            beer.name.toLowerCase().includes(search) ||
+            beer.brewery.toLowerCase().includes(search) ||
+            beer.style.toLowerCase().includes(search)
+          );
+
+        })
+
+    }))
+    .filter(section => section.beers.length > 0);
+
+  });
+  readonly categories = computed(() => [
+    'All',
+    ...this.sections().map(s => s.title)
+  ]);
   ngOnInit(): void {
 
     this.slideService.getSlides().subscribe(slides => {
---
+++ src/app/services/layout-mode.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LayoutModeService {
+
+  readonly isMobile = signal(window.innerWidth < 768);
+
+  constructor() {
+
+    window.addEventListener('resize', () => {
+      this.isMobile.set(window.innerWidth < 768);
+    });
+
+  }
+
+}
